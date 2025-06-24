be50991d195c42d87c17131cc4cc1c0f283fca25
Tridots <info@tridotstech.com>
2025-06-24 12:52:32 +0530

    refactor:changing url

---
+++ gscommunity/templates/pages/membership_payment.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+# import frappe
+# from _future_ import unicode_literals
+import frappe
+import frappe.utils
+import frappe.handler
+import frappe.client
+from frappe.utils.oauth import get_oauth2_authorize_url, get_oauth_keys, login_via_oauth2, login_oauth_user as _login_oauth_user, redirect_post_login
+from frappe.utils.response import build_response
+import json
+import calendar
+from frappe import _
+from frappe.utils import getdate,nowdate
+from datetime import date
+import requests
+import braintree
+import werkzeug
+
+gateway = braintree.BraintreeGateway(
+  braintree.Configuration(
+    environment=braintree.Environment.Production,
+    merchant_id=frappe.db.get_single_value('Braintree Integration','merchant_id'),
+    public_key=frappe.db.get_single_value('Braintree Integration','public_key'),
+    private_key=frappe.db.get_single_value('Braintree Integration','private_key')
+  )
+)
+
+@frappe.whitelist(allow_guest=True)
+def generate_client_token():	
+	return gateway.client_token.generate()
